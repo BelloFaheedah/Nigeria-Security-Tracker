d7ff590d28d5e58a423df2ca0f396d53a452ea9d
Faheedah Bukola Bello <54367030+BelloFaheedah@users.noreply.github.com>
2022-12-05 04:15:01 +0100

    Create r-script.R

---
+++ scripts/r-script.R
@@ -0,0 +1,6 @@
+library(usethis)
+library(googlesheets4)
+
+df_sheet_1 <- read_sheet("https://docs.google.com/spreadsheets/d/1_QY-l4xhMu5nZVluprOgRs6rUzgkkBemapdsg5lFzKU/pub?output=xlsx")
+df_sheet_1
+
